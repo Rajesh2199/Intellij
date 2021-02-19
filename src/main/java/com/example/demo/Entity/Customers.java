966e35bdaac973c120571bc01e9ac4c36c2925d0
baduraje@isu.edu <baduraje@isu.edu>
2021-02-19 08:24:53 -0700

    added default constructor to the entity

--- src/main/java/com/example/demo/Entity/Customers.java
+++ src/main/java/com/example/demo/Entity/Customers.java
@@ -1,5 +1,11 @@
 package com.example.demo.Entity;
 
 public class Customers {
+            String firstname;
+            String lastname;
 
+    public Customers() {
+        // This is a default constructor
+
+    }
 }
